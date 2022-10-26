d1a57b3f8f1f805ca0f53167f1987497dfd9b4ae
Revant Adlakha <revant4410@gmail.com>
2022-10-25 23:36:11 -0400

    added mkdir

--- record_audio.py
+++ record_audio.py
@@ -1,8 +1,13 @@
 from src.utils import *
+import os
 
 chunk = 1024
 seconds = 10
 RAobject = record_audio(chunk, seconds)
 RAobject.recordAudio()
 RAobject.stopRecording()
+
+if not os.path.dirname('audio_recordings'):
+    os.makedirs('audio_recordings/')
+
 RAobject.saveWave('audio_recordings/test.wav')
