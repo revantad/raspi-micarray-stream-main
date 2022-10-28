77aaaa988cd35f81bce9ddd5f42cde2b44e8b1e3
Revant Adlakha <revant4410@gmail.com>
2022-10-27 20:34:12 -0400

    f

--- src/utils.py
+++ src/utils.py
@@ -31,7 +31,9 @@ class record_audio():
         for ii in range(0,int((self.samp_rate/self.chunk)*self.record_secs)):
             data = self.stream.read(self.chunk)
             
-            print('Recording and saving with fft, ifft')
+            if ii == 0:
+                print('Recording and saving with fft, ifft')
+            
             frame2.append(np.fft.ifft(np.fft.fft(np.frombuffer(data))))
             #frames.append(data)
 
