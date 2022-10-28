69ae94399d8716088da3c3356b8b1ff72d715788
Revant Adlakha <revant4410@gmail.com>
2022-10-28 18:00:17 -0400

    f

--- src/utils.py
+++ src/utils.py
@@ -42,7 +42,7 @@ class record_audio():
         
         self.frames = frames
         self.frames_dat = frames_dat
-        print(len(self.frames_dat), len(self.frames))
+        print(len(self.frames_dat[0]), len(self.frames))
         self.stopRecording()
         
 
