2118f0f3c10f5e956207020baab2861c10dbc2a0
Revant Adlakha <revant4410@gmail.com>
2022-10-28 17:24:38 -0400

    f

--- src/utils.py
+++ src/utils.py
@@ -34,8 +34,8 @@ class record_audio():
             
             data_float = np.fromstring(data, dtype = np.float)
             if ii == 1:
-                ddat = np.fft.ifft(np.fft.fft(data_float, n = int(self.chunk//2 + 1)), n = int(self.chunk))
-                print(np.size(ddat), np.size(data_float), data.shape())
+                ddat = np.fft.ifft(np.fft.fft(data_float, n = int(self.chunk//2 + 1)), n = int(2*self.chunk))
+                print(np.size(ddat), np.size(data_float))
                 
             #frame2.append(np.fft.fft(np.fft.fft(data_float, n = int(self.chunk/2 + 1)), n = int(self.chunk)))
             frame2.append(data_float)
