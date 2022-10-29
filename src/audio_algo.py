a71549baf45a4dc00e66c521e836bffe1239cc9a
Revant Adlakha <revant4410@gmail.com>
2022-10-28 23:10:37 -0400

    frame

--- src/audio_algo.py
+++ src/audio_algo.py
@@ -15,13 +15,13 @@ class beamformer():
         bf_out = np.zeros(shape = [self.channels, self.nfft], dtype = np.complex64)
         
         for k in range(0, self.nfft):
-            R[:, :, k] = np.outer(mic_synth[:, k], np.conjugate(mic_synth[:, k]))
+            R[:, :, k] = np.outer(frame[:, k], np.conjugate(frame[:, k]))
             eig_val, eig_vec = np.linalg.eig(R[:, :, k])
             max_index = np.argmax(eig_val)
             atf[:, k] = eig_vec[max_index]
             R_inv[:, :, k] = np.linalg.inv(R[:, :, k])
 
             w[:, k] = np.matmul(np.R_inv[:, :, k], np.conjugate(atf[:, k]))/(np.matmul(atf[:, 1], np.matmul(R_inv, np.conjugate(atf[:, k]))))
-            bf_out[:, k] = np.matmul(w[:, k], mic_synth[:, k])
+            bf_out[:, k] = np.matmul(w[:, k], frame[:, k])
     
         return bf_out    
