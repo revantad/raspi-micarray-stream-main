34b13c107f5c7f6b26e1e5fa20ba4c1ee8cccae1
Revant Adlakha <revant4410@gmail.com>
2022-10-28 23:33:22 -0400

    f

--- src/audio_algo.py
+++ src/audio_algo.py
@@ -13,16 +13,19 @@ class beamformer():
 
     def process(self, frame):
         # Initialize
+        # self.R = np.outer(frame, np.conjugate(frame), axis = 2)
         for k in range(0, self.nfft):
-            self.R[:, :, k] = np.outer(frame[:, k], np.conjugate(frame[:, k]))
+            self.R[:, :, k] = np.outer(frame[:, k], np.conjugate(frame[:, k]), self.R[:, :, k])
             eig_val, eig_vec = np.linalg.eig(self.R[:, :, k])
             max_index = np.argmax(eig_val)
             self.atf[:, k] = eig_vec[max_index]
             self.R_inv[:, :, k] = np.linalg.pinv(self.R[:, :, k])
             #print(np.shape(R), np.shape(R_inv), np.shape(atf))
-            self.alpha[k] = np.matmul(self.atf[:, k], np.matmul(self.R_inv[:, :, k], np.conjugate(self.atf[:, k])))
+            temp = np.matmul(self.R_inv[:, :, k], np.conjugate(self.atf[:, k]))
+            self.alpha[k] = np.matmul(self.atf[:, k], temp, self.alpha[k])
             #print(alpha)
-            self.w[:, k] = np.matmul(self.R_inv[:, :, k], np.conjugate(self.atf[:, k]))/self.alpha[k]
-            self.bf_out[k] = np.inner(self.w[:, k], np.conjugate(frame[:, k]))
+            self.w[:, k] = np.matmul(self.R_inv[:, :, k], np.conjugate(self.atf[:, k]), self.w[:, k])
+            self.w[:, k] = self.w[:, k]/self.alpha[k]
+            self.bf_out[k] = np.inner(self.w[:, k], np.conjugate(frame[:, k]), self.bf_out[:, k])
         
         return self.bf_out    
