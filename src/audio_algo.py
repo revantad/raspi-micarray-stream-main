cb9ef7c736db632fadd10db341286cf4bc5eb3e2
Revant Adlakha <revant4410@gmail.com>
2022-10-28 23:19:55 -0400

    self

--- src/audio_algo.py
+++ src/audio_algo.py
@@ -4,25 +4,25 @@ class beamformer():
     def __init__(self, nfft, channels):
         self.nfft = nfft
         self.channels = channels
-    
+        self.R = np.zeros(shape = [self.channels, self.channels, self.nfft], dtype = np.complex64)
+        self.R_inv = np.zeros(shape = [self.channels, self.channels, self.nfft], dtype = np.complex64)
+        self.atf = np.zeros(shape = [self.channels, self.nfft], dtype = np.complex64)
+        self.w = np.zeros(shape = [self.channels, self.nfft], dtype = np.complex64)
+        self.bf_out = np.zeros(shape = [self.channels, self.nfft], dtype = np.complex64)
+        self.alpha = np.zeros(shape = [self.nfft], dtype = np.complex64)
+
     def process(self, frame):
         # Initialize
-        R = np.zeros(shape = [self.channels, self.channels, self.nfft], dtype = np.complex64)
-        R_inv = np.zeros(shape = [self.channels, self.channels, self.nfft], dtype = np.complex64)
-        atf = np.zeros(shape = [self.channels, self.nfft], dtype = np.complex64)
-        w = np.zeros(shape = [self.channels, self.nfft], dtype = np.complex64)
-        bf_out = np.zeros(shape = [self.channels, self.nfft], dtype = np.complex64)
-        
         for k in range(0, self.nfft):
-            R[:, :, k] = np.outer(frame[:, k], np.conjugate(frame[:, k]))
-            eig_val, eig_vec = np.linalg.eig(R[:, :, k])
+            self.R[:, :, k] = np.outer(frame[:, k], np.conjugate(frame[:, k]))
+            eig_val, eig_vec = np.linalg.eig(self.R[:, :, k])
             max_index = np.argmax(eig_val)
-            atf[:, k] = eig_vec[max_index]
-            R_inv[:, :, k] = np.linalg.pinv(R[:, :, k])
+            self.atf[:, k] = eig_vec[max_index]
+            self.R_inv[:, :, k] = np.linalg.pinv(self.R[:, :, k])
             #print(np.shape(R), np.shape(R_inv), np.shape(atf))
-            alpha = np.matmul(atf[:, k], np.matmul(R_inv[:, :, k], np.conjugate(atf[:, k])))
+            self.alpha[k] = np.matmul(self.atf[:, k], np.matmul(self.R_inv[:, :, k], np.conjugate(self.atf[:, k])))
             #print(alpha)
-            w[:, k] = np.matmul(R_inv[:, :, k], np.conjugate(atf[:, k]))/alpha
-            bf_out[:, k] = np.matmul(w[:, k], frame[:, k])
+            self.w[:, k] = np.matmul(self.R_inv[:, :, k], np.conjugate(self.atf[:, k]))/self.alpha[k]
+            self.bf_out[:, k] = np.matmul(self.w[:, k], frame[:, k])
     
-        return bf_out    
+        return self.bf_out    
