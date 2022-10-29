41afe4505bb254c6d0a2341fddd11fcb30a5c435
Revant Adlakha <revant4410@gmail.com>
2022-10-29 00:52:35 -0400

    f

--- src/audio_algo.py
+++ src/audio_algo.py
@@ -41,7 +41,7 @@ class beamformer():
 
         for k in range(0, self.nfft):
             self.alpha[k] = np.matmul(np.conjugate(w_temp[k, :]), atf[k, :])
-            self.bf_out[k] = np.matmul(w_temp[k, :], np.conjugate(frame))/alpha[k]
+            self.bf_out[k] = np.matmul(w_temp[k, :], np.conjugate(frame[k, :]))/alpha[k]
 
         
         return bf_out
