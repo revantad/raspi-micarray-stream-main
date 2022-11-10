ae0f07e7f84b69fb0125a4908daea0e24da564fe
Revant Adlakha <revant4410@gmail.com>
2022-11-10 14:52:13 -0500

    f

---
+++ record_audio.pyx
@@ -0,0 +1,21 @@
+import os
+import time
+from src.utils import *
+from src.audio_algo import *
+
+chunk = 1024
+seconds = 10
+samp_rate = 48e3
+num_channels = 4
+audObject = record_audio(chunk, seconds, samp_rate, num_channels)
+
+start = time.time()
+audObject.recordAudio()
+audObject.stopRecording()
+print('Time ' + str(time.time() - start))
+print('Requested Time ' + str(seconds))
+
+if not os.path.dirname('audio_recordings/'):
+    os.makedirs('audio_recordings/')
+
+audObject.saveWave('audio_recordings/test.wav')
