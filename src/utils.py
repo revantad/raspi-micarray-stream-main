ec5aed9eacf1a88e0b4aa8892f822f7609a2712f
Revant Adlakha <revant4410@gmail.com>
2022-11-16 22:51:38 -0500

    f

--- src/utils.py
+++ src/utils.py
@@ -41,11 +41,11 @@ class record_audio():
             
             # Convert float data to matrix of size [channels x frame samples]
             mic_frames = np.reshape(data_float, [self.chans, self.chunk])
-            mic_analy = np.fft.rfft(mic_frames, axis = 1, n = 2*self.nfft - 1)
+            mic_analy = np.fft.fft(mic_frames, axis = 1, n = 2*self.nfft - 1)
 
             ## Call audio algorithms/pipeline here
             bf_analy = self.bf.process(mic_analy)
-            bf_synth = np.real(np.fft.irfft(bf_analy, axis = 0, n = self.chunk))
+            bf_synth = np.real(np.fft.ifft(bf_analy, axis = 0, n = self.chunk))
             bf_dat[ii*(self.bf_channel*self.chunk):(ii + 1)*(self.bf_channel*self.chunk)] = bf_synth
 
             mic_synth = np.real(np.fft.irfft(mic_analy, axis = 1, n = self.chunk))
