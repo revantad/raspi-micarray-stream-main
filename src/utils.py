7dd18e1bd835fa5c67e6647a24f55641a94be684
Revant Adlakha <revant4410@gmail.com>
2022-11-16 21:46:44 -0500

    f

--- src/utils.py
+++ src/utils.py
@@ -45,7 +45,7 @@ class record_audio():
 
             ## Call audio algorithms/pipeline here
             bf_analy = self.bf.process(mic_analy)
-            bf_synth = np.fft.irfft(bf_analy, axis = 0, n = self.chunk)
+            bf_synth = np.real(np.fft.irfft(bf_analy, axis = 1, n = self.chunk))
             bf_dat[ii*(self.bf_channel*self.chunk):(ii + 1)*(self.bf_channel*self.chunk)] = bf_synth
 
             mic_synth = np.real(np.fft.irfft(mic_analy, axis = 1, n = self.chunk))
