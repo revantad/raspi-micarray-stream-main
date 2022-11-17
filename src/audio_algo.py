5780fb16d8863342dbf60d057790c28b35942f8d
Revant Adlakha <revant4410@gmail.com>
2022-11-16 22:06:37 -0500

    f

--- src/audio_algo.py
+++ src/audio_algo.py
@@ -18,12 +18,12 @@ class beamformer():
         w_temp = np.squeeze(np.matmul(R_inv, np.reshape(atf, [self.nfft, self.channels, 1])))
         
         #start = time.time()
-        #for k in range(0, self.nfft):
-        #    self.alpha[k] = np.matmul(np.conjugate(w_temp[k, :]), atf[k, :])
-        #    self.bf_out[k] = np.matmul(w_temp[k, :], np.conjugate(frame[k, :]))/self.alpha[k]
+        for k in range(0, self.nfft):
+            self.alpha[k] = np.matmul(np.conjugate(w_temp[k, :]), atf[k, :])
+            self.bf_out[k] = np.matmul(w_temp[k, :], np.conjugate(frame[k, :]))/self.alpha[k]
 
-        self.alpha = np.multiply(np.conjugate(w_temp[:, 0]), atf[:, 0]) + np.multiply(np.conjugate(w_temp[:, 1]), atf[:, 1]) + np.multiply(np.conjugate(w_temp[:, 2]), atf[:, 2]) + np.multiply(np.conjugate(w_temp[:, 3]), atf[:, 3])
-        self.bf_out = np.multiply(np.multiply(w_temp[:, 0], np.conjugate(frame[:, 0])) + np.multiply(w_temp[:, 1], np.conjugate(frame[:, 1])) + np.multiply(w_temp[:, 2], np.conjugate(frame[:, 2])) + np.multiply(w_temp[:, 3], np.conjugate(frame[:, 3])), 1/( self.eps + self.alpha))
+        #self.alpha = np.multiply(np.conjugate(w_temp[:, 0]), atf[:, 0]) + np.multiply(np.conjugate(w_temp[:, 1]), atf[:, 1]) + np.multiply(np.conjugate(w_temp[:, 2]), atf[:, 2]) + np.multiply(np.conjugate(w_temp[:, 3]), atf[:, 3])
+        #self.bf_out = np.multiply(np.multiply(w_temp[:, 0], np.conjugate(frame[:, 0])) + np.multiply(w_temp[:, 1], np.conjugate(frame[:, 1])) + np.multiply(w_temp[:, 2], np.conjugate(frame[:, 2])) + np.multiply(w_temp[:, 3], np.conjugate(frame[:, 3])), 1/( self.eps + self.alpha))
 
         #print('Time: ' + str(time.time() - start))
         
