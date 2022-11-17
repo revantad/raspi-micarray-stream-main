55fc926628a1ef24727eeb137b77939c8b8efc98
Revant Adlakha <revant4410@gmail.com>
2022-11-16 20:55:31 -0500

    f

--- src/utils.py
+++ src/utils.py
@@ -44,9 +44,9 @@ class record_audio():
             mic_analy = np.fft.rfft(mic_frames, axis = 1, n = 2*self.nfft - 1)
 
             ## Call audio algorithms/pipeline here
-            #bf_analy = self.bf.process(mic_analy)
-            #bf_synth = np.fft.irfft(bf_analy, axis = 0, n = self.chunk)
-            #bf_dat[ii*(self.bf_channel*self.chunk):(ii + 1)*(self.bf_channel*self.chunk)] = bf_synth
+            bf_analy = self.bf.process(mic_analy)
+            bf_synth = np.fft.irfft(bf_analy, axis = 0, n = self.chunk)
+            bf_dat[ii*(self.bf_channel*self.chunk):(ii + 1)*(self.bf_channel*self.chunk)] = bf_synth
 
             mic_synth = np.real(np.fft.irfft(mic_analy, axis = 1, n = self.chunk))
             mic_synth_flat = np.reshape(mic_synth, [1, len(data_float)])
