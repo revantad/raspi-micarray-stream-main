954d7bf24be0ff708f8a4107c85248aabff7caab
Revant Adlakha <revant4410@gmail.com>
2022-11-16 23:32:47 -0500

    f

--- src/utils.py
+++ src/utils.py
@@ -48,7 +48,7 @@ class record_audio():
 
             ## Call audio algorithms/pipeline here
             bf_frames = np.reshape(data_float2, [self.chans, self.chunk])
-            bf_mic_analy = np.fft.fft(bf_frames, axis = 1, n = self.nfft)
+            bf_mic_analy = np.fft.fft(bf_frames, axis = 1, n = 2*self.nfft)
             #print(bf_mic_analy)
             bf_analy = self.bf.process(bf_mic_analy)
             #print(bf_analy)
