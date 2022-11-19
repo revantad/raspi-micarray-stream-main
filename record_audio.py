f729606fdd55a1588d3e83472f381689311f3046
Revant Adlakha <revant4410@gmail.com>
2022-11-18 20:34:51 -0500

    f

--- record_audio.py
+++ record_audio.py
@@ -3,9 +3,9 @@ import time
 from src.utils import *
 from src.audio_algo import *
 
-chunk = 512
+chunk = 256
 seconds = 10
-samp_rate = 8e3
+samp_rate = 16e3
 num_channels = 4
 audObject = record_audio(chunk, seconds, samp_rate, num_channels)
 
