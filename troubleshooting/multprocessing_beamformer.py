e5a88539031d40ca206bf9f2d0d308b1516f5b37
Revant Adlakha <revant4410@gmail.com>
2022-12-07 19:38:54 -0500

    f

--- troubleshooting/multprocessing_beamformer.py
+++ troubleshooting/multprocessing_beamformer.py
@@ -15,7 +15,7 @@ class beamformer_multi():
     def process2(self, frame):
         start = time.time()
 
-        processes = [multiprocessing.Process(target=task(frame)) for _ in range(4)]
+        processes = [multiprocessing.Process(target=self.task(frame)) for _ in range(4)]
         [process.start() for process in processes]
         [process.join() for process in processes]
         print('Time: ' + str(time.time() - start))
