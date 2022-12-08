b3e8b934229fd6724324350f7266806144071be4
Revant Adlakha <revant4410@gmail.com>
2022-12-07 22:25:48 -0500

    f

--- troubleshooting/multprocessing_beamformer.py
+++ troubleshooting/multprocessing_beamformer.py
@@ -3,6 +3,7 @@ import time
 import multiprocessing
 import threading
 import os
+from Cython.Build import cythonize
 
 import concurrent.futures
 
@@ -15,6 +16,23 @@ class beamformer_multi():
         self.alpha = np.zeros(shape = [self.nfft], dtype = np.complex)
         self.eps = 1e-16
     
+    def process(self, frame):
+        #start = time.time()
+        
+        # init
+        self.frame = frame.T # nfft x channels
+        self.R = np.zeros(shape = [self.nfft, self.channels, self.channels], dtype = np.complex)
+        self.R_inv = np.zeros(shape = [self.nfft, self.channels, self.channels], dtype = np.complex)
+        self.atf = np.zeros(shape = [self.nfft, self.channels], dtype = np.complex)
+        self.w_temp = np.zeros(shape = [self.nfft, self.channels], dtype = np.complex)
+
+
+        for ind in range(0, self.nfft):
+                self.bf_out[ind], _ = self.task(ind)
+
+        #print('Time: ' + str(time.time() - start))
+        return self.bf_out
+
     def process2(self, frame):
         #start = time.time()
         
