7286f6040e389ea56353d50115e8e8a2206ecb5a
Revant Adlakha <revant4410@gmail.com>
2022-12-07 19:38:34 -0500

    f

--- troubleshooting/multprocessing_beamformer.py
+++ troubleshooting/multprocessing_beamformer.py
@@ -13,34 +13,30 @@ class beamformer_multi():
         self.eps = 1e-16
     
     def process2(self, frame):
+        start = time.time()
+
+        processes = [multiprocessing.Process(target=task(frame)) for _ in range(4)]
+        [process.start() for process in processes]
+        [process.join() for process in processes]
+        print('Time: ' + str(time.time() - start))
+
+        return self.bf_out
+
+    def task(self, frame):
         frame = frame.T # nfft x channels
         R = np.zeros(shape = [self.nfft, self.channels, self.channels], dtype = np.complex)
         R_inv = np.zeros(shape = [self.nfft, self.channels, self.channels], dtype = np.complex)
         atf = np.zeros(shape = [self.nfft, self.channels], dtype = np.complex)
         w_temp = np.zeros(shape = [self.nfft, self.channels], dtype = np.complex)
-
-        start = time.time()
-
-        processes = [multiprocessing.Process(target=task) for _ in range(4)]
-        [process.start() for process in processes]
-        [process.join() for process in processes]
-        print('Time: ' + str(time.time() - start))
-
-
-        def task():
-            for k in range(0, self.nfft):
-                curr_frame = frame[k, :]
-                R[k, :, :] = curr_frame.T * curr_frame # [nfft x channels x channels]
-                R_inv[k, :, :] = np.linalg.pinv(self.eps + R[k, :, :]) # [nfft x channels x channels]
-                _, eig_vecs = np.linalg.eigh(np.squeeze(R[k, :, :]))
-                atf[k, :] = eig_vecs[0, :]
+        for k in range(0, self.nfft):
+            curr_frame = frame[k, :]
+            R[k, :, :] = curr_frame.T * curr_frame # [nfft x channels x channels]
+            R_inv[k, :, :] = np.linalg.pinv(self.eps + R[k, :, :]) # [nfft x channels x channels]
+            _, eig_vecs = np.linalg.eigh(np.squeeze(R[k, :, :]))
+            atf[k, :] = eig_vecs[0, :]
                             
-                w_temp[k, :] = np.matmul(R_inv[k, :, :], atf[k, :], out = w_temp[k, :])
-                self.alpha[k] = np.matmul(np.conjugate(w_temp[k, :]), atf[k, :])
-                self.bf_out[k] = np.matmul(w_temp[k, :], np.conjugate(curr_frame))/(self.eps + self.alpha[k])
-
-        
-        return self.bf_out
-
+            w_temp[k, :] = np.matmul(R_inv[k, :, :], atf[k, :], out = w_temp[k, :])
+            self.alpha[k] = np.matmul(np.conjugate(w_temp[k, :]), atf[k, :])
+            self.bf_out[k] = np.matmul(w_temp[k, :], np.conjugate(curr_frame))/(self.eps + self.alpha[k])
 
 
